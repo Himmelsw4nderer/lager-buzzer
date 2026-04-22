e5ebd4a1e91d7e84275410b2ba5ef7b2983f9a4c
nbax <Nils>
2026-04-22 17:05:12 +0200

    Adding that the controller identifies the buzzer id to abstract the mac adress

--- firmware/controller/src/main.cpp
+++ firmware/controller/src/main.cpp
@@ -1,10 +1,24 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <QuickEspNow.h>
+#include "buzzer_config.h"
+
+int16_t lookupBuzzerId(const uint8_t* mac) {
+    for (size_t i = 0; i < BUZZER_COUNT; i++) {
+        if (memcmp(mac, BUZZER_MACS[i], 6) == 0) {
+            return BUZZER_IDS[i];
+        }
+    }
+    return -1;
+}
 
 void onDataReceived(uint8_t* mac, uint8_t* data, uint8_t len, signed int rssi, bool broadcast) {
-    Serial.printf("[CONTROLLER] Received %d bytes from " MACSTR " (RSSI: %d)\n",
-                  len, MAC2STR(mac), rssi);
+    int16_t id = lookupBuzzerId(mac);
+    if (id < 0) {
+        Serial.printf("[CONTROLLER] Received from unknown device " MACSTR "\n", MAC2STR(mac));
+    } else {
+        Serial.printf("[CONTROLLER] Received from buzzer ID=%d\n", id);
+    }
 }
 
 void setup() {
